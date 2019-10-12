34c3a417e50903f110b6edc74c96abe17467a0a6
victoria <kapitonenko.vick@ya.ru>
2019-10-12 12:43:00 +0300

    chore: add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,35 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ['error', 2],
+    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
+    'quote-props': 0,
+    'comma-dangle': 0,
+    'import/no-unresolved': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'no-unused-expressions': ['error', { 'allowTernary': true }],
+    'prefer-destructuring': ['error', { 'array': false, 'object': true }],
+    'max-len': ['error', {
+      'code': 100,
+      'ignoreTemplateLiterals': true,
+      'ignoreStrings': true,
+      'ignoreRegExpLiterals': true,
+      'ignoreUrls': true,
+      'ignorePattern': '^<path[\\s\\S]*\\/>$'
+    }],
+    'vue-a11y/click-events-have-key-events': 0,
+    'space-before-function-paren': 0,
+    'semi': 0
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'indent': 'off'
+      }
+    }
+  ]
 }
