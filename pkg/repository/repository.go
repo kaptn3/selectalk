8b2ac0892e576bc2aef8dff596b35aa25881a1d1
Max Isaev <isaevm2@yandex.ru>
2019-10-12 19:53:10 +0300

    update package repository

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -18,10 +18,30 @@ func NewUserRepository() *UserRepository {
 
 func (r *UserRepository) FindByID(id int) (model.User, error) {
 	var user model.User
-	result := r.db.Where("id = ?", id).Take(&user)
-	if result.Error != nil {
-		return user, result.Error
+	res := r.db.Where("id = ?", id).Take(&user)
+	if res.Error != nil {
+		return user, res.Error
 	}
 
 	return user, nil
+}
+
+type TaskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository() *TaskRepository {
+	return &TaskRepository{
+		db: database.GetInstance(),
+	}
+}
+
+func (r TaskRepository) FindByUserID(userID int) ([]model.Task, error) {
+	var tasks []model.Task
+	res := r.db.Where("user_id = ?", userID).Find(&tasks)
+	if res.Error != nil {
+		return tasks, res.Error
+	}
+
+	return tasks, nil
 }
