88bbcc895f2acd10065edabb04094993ce8f4843
victoria <kapitonenko.vick@ya.ru>
2019-10-12 23:00:17 +0300

    feat: add courses

---
+++ components/courses/Card.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="card">
+    <div class="card__img">
+      <img :src="img">
+    </div>
+    <div class="card__text">
+      <h3 class="card__title">
+        {{ title }}
+      </h3>
+      <span class="card__type">
+        {{ type }}
+      </span>
+    </div>
+    <div class="card__progress">
+      {{ percent }}%
+      <div
+        :style="{ 'transform': `rotate(${360 * percent / 100}deg)` }"
+        class="card__circle"
+      />
+      <div class="card__circle"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+    props: {
+      img: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      percent: {
+        type: Number,
+        required: true
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .card {
+    background-color: var(--white);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, .15);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    font-size: 14px;
+  }
+
+  .card__img {
+    width: 100%;
+    height: 170px;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .card__progress {
+    font-size: 11px;
+    width: 28px;
+    height: 28px;
+    border: 1px solid var(--red);
+    border-radius: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    margin: 20px 20px 20px 0;
+  }
+
+  .card__circle {
+    width: 26px;
+    height: 26px;
+    border: 1px solid var(--red);
+    border-radius: 100%;
+    flex-shrink: 0;
+    position: absolute;
+    clip: rect(0, 13px, 26px, 0);
+    left: 0;
+    top: 0;
+    transform: rotate(180deg);
+  }
+
+  .card__text {
+    padding: 20px 0 20px 20px;
+  }
+
+  span {
+    opacity: .5;
+  }
+</style>
--- pages/courses.vue
+++ pages/courses.vue
@@ -1,11 +1,72 @@
 <template>
-  <div>
-    Курсы
+  <div class="courses">
+    <card
+      v-for="course in courses"
+      :key="course.title"
+      :percent="60"
+      :img="course.img"
+      :title="course.title"
+      :type="course.type"
+      class="courses__item"
+    />
   </div>
 </template>
 
 <script>
+  import Card from '../components/courses/Card';
+
   export default {
-    layout: 'lists'
+    layout: 'lists',
+    components: { Card },
+    data() {
+      return {
+        courses: [
+          {
+            img: '/img/1.png',
+            title: 'Сетевая безопасность',
+            type: 'Упражнение'
+          },
+          {
+            img: '/img/2.png',
+            title: 'Размещение серверов',
+            type: 'Статья'
+          },
+          {
+            img: '/img/3.png',
+            title: 'Общение с поставщиками',
+            type: 'Тест'
+          },
+          {
+            img: '/img/4.png',
+            title: 'Безопасность в Data центрах',
+            type: 'Курс'
+          },
+          {
+            img: '/img/5.png',
+            title: 'Сотрудники Data центров',
+            type: 'Видео'
+          }
+        ]
+      };
+    }
   };
 </script>
+
+<style scoped>
+  .courses {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    margin-top: 50px;
+  }
+
+  .courses__item {
+    width: 31%;
+    margin-right: 3%;
+    margin-bottom: 40px;
+  }
+
+  .courses__item:nth-child(3n) {
+    margin-right: 0;
+  }
+</style>
