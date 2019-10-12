992091cb06da1f5b8987d40d27e1356ef9fb8034
Max Isaev <isaevm2@yandex.ru>
2019-10-12 22:43:17 +0300

    create migrations for tables achievement and course

---
+++ deployments/migrations/postgres/1570904918_fill_achievement.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO achievement
+ (id, created_at, updated_at, "name", cost)
+VALUES
+  (DEFAULT, now(), now(), 'Командировка в Москву', 30000),
+  (DEFAULT, now(), now(), 'Конференция в Казани', 15000),
+  (DEFAULT, now(), now(), 'Английский язык', 13500),
+  (DEFAULT, now(), now(), 'Участие в эксперименте', 11500),
+  (DEFAULT, now(), now(), 'Поддержка проекта', 40000);
---
+++ deployments/migrations/postgres/1570906737_fill_course.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO course
+  (id, created_at, updated_at, "name", kind, award)
+VALUES
+  (DEFAULT, now(), now(), 'Сетевая безопасность', 'Упражнение', 0),
+  (DEFAULT, now(), now(), 'Размещение серверов', 'Статья', 0),
+  (DEFAULT, now(), now(), 'Общение с постащиками', 'Тест', 0),
+  (DEFAULT, now(), now(), 'Безопасность в дата центре', 'Курс', 0),
+  (DEFAULT, now(), now(), 'Сотрудники в дата центре', 'Видео', 0);
