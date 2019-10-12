dad3c6c65ec5cc717e95699b94ac46d6f2bbdc1c
Max Isaev <isaevm2@yandex.ru>
2019-10-12 18:38:45 +0300

    edit migration fill_user_table

--- deployments/migrations/postgres/1570883312_fill_user.up.sql
+++ deployments/migrations/postgres/1570883312_fill_user.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO "user"
-  (id, balance, account, skill, department)
-VALUES (DEFAULT, 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'engineering');
+  (id, "name", updated_at, created_at, balance, account, skill, "position", department)
+VALUES ( DEFAULT, 'Мария Луганова', now(), now(), 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'Главный менеджер', 'Инноваций и менеджмента');
