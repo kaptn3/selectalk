3dfc3a59b71ecab44deb7f5d2b0ec921d1344d81
Max Isaev <isaevm2@yandex.ru>
2019-10-12 22:43:08 +0300

    fix migration for the user table

--- deployments/migrations/postgres/1570883312_fill_user.up.sql
+++ deployments/migrations/postgres/1570883312_fill_user.up.sql
@@ -1,3 +1,5 @@
 INSERT INTO "user"
   (id, "name", updated_at, created_at, balance, account, skill, "position", department)
-VALUES ( DEFAULT, 'Мария Луганова', now(), now(), 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'Главный менеджер', 'Инноваций и менеджмента');
+VALUES
+  (DEFAULT, 'Мария', now(), now(), 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'Главный менеджер', 'Инноваций и менеджмента'),
+  (DEFAULT, 'Дмитрий', now(), now(), 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'Руководитель подразделение', 'Инноваций и менеджмента');
