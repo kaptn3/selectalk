9888c32a3164debdab7811e5b03055724b276b64
Max Isaev <isaevm2@yandex.ru>
2019-10-12 15:42:22 +0300

    add seeders

---
+++ deployments/migrations/postgres/1570883312_fill_user.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user"
+  (id, balance, account, skill, department)
+VALUES (DEFAULT, 0.0, '0x16', '{"accuracy": 0.0, "responsibility": 0.0, "sociability": 0.0, "punctuality": 0.0, "observation": 0.0}', 'engineering');
