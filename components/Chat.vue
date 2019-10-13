2866157acd95e4d7eea091980acfb2b555e8f8ba
victoria <kapitonenko.vick@ya.ru>
2019-10-13 06:12:34 +0300

    feat: add chat

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,16 +1,46 @@
 <template>
-  <div/>
+  <div>
+    <button @click="display = 'block'"/>
+    <img
+      :style="{ 'display': display }"
+      src="/img/chat.svg"
+      alt=""
+      @click="display = 'none'"
+    >
+  </div>
 </template>
 
+<script>
+  export default {
+    name: 'Chat',
+    data() {
+      return {
+        display: 'none'
+      }
+    }
+  };
+</script>
+
 <style scoped>
-  div {
+  button {
     box-shadow: 0 4px 10px rgba(0, 0, 0, .5);
     width: 60px;
     height: 60px;
     background: var(--black) url(/icons/chat.png) no-repeat center;
+    border-radius: 100%;
+  }
+
+  div {
     position: fixed;
     bottom: 42px;
     right: 42px;
-    border-radius: 100%;
+  }
+
+  img {
+    max-width: initial;
+    position: absolute;
+    right: -8px;
+    bottom: -8px;
+    cursor: pointer;
   }
 </style>
